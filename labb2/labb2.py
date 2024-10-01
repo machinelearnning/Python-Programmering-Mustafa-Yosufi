760d42df2fcb2c9f53d412ddd5b52833f1b61749
Mustafa Yosufi <mustafayusefi45@gamil.com>
2024-10-01 09:02:17 +0200

    created funktion in labb2

--- labb2/labb2.py
+++ labb2/labb2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv("datapoints.txt")
 df.columns = df.columns.str.replace("(", "").str.replace(")", "").str.strip().str.replace(" ", "_").str.capitalize()
@@ -25,7 +26,36 @@ test_data["Width_cm"] = test_data["test_d"].str.split("(").str.get(1).str.split(
 test_data["Height_cm"] = test_data["test_d"].str.split(")").str.get(0).str.split(",").str.get(1)
 test_data = test_data.drop("test_d", axis= 1)
 test_data[["Height_cm", "Width_cm"]] = test_data[["Height_cm", "Width_cm"]].astype(float)
-print(test_data)
 
 
+def clasify_point(maj, data_x , data_y,pic_pika, wid, hei):
+    points_distans =[(np.sqrt(np.power(x- wid, 2) + np.power(y- hei, 2))) 
+                     for x, y in zip(data_x, data_y)]
+    
+    if maj == 1:
 
+        min_value = min(points_distans)
+        index_value = points_distans.index(min_value)
+    
+        if pic_pika[index_value] != 0:
+            return 1
+        else:
+            return 0
+    
+    elif maj == 10:
+        sort_values = sorted(points_distans)
+        ten_values = sort_values[:10]
+        index_lis_values = [points_distans.index(x) for x in ten_values]
+        choose_maj = sum([pic_pika[x] for x in index_lis_values])
+
+        if choose_maj > 5:
+            return 1
+        else:
+            return 0
+        
+    else:
+        print("Enter 1 or 10")
+        
+# köra test data med df som tränings data
+for x , y in zip(test_data["Width_cm"], test_data["Height_cm"]):
+    print(clasify_point(1, df["Width_cm"], df["Height_cm"], df["Pichu-0_pikachu-1"],x, y))
